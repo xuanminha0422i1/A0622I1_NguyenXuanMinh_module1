2eca86889f31e4d3685fadb02319d475ceb029f6
unknown <you@example.com>
2022-08-07 15:09:32 +0700

    first commit

---
+++ ss14_Hàm 1/thuc_hanh/script.js
@@ -0,0 +1,15 @@
+function square(number) {
+ var result = number * number;
+ return result;
+}
+// var square0f2 = square(2);
+// document.write("square of 2 is: " + square0f2 + "<br/>");
+//
+// var square0f5 = square(5);
+// document.write("square of 5 is: " + square0f5);
+function calculate() {
+ var input = document.getElementById("input").value;
+ var result = square(input);
+ document.getElementById("result").innerHTML = result;
+
+}
